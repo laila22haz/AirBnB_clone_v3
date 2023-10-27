164409890f36584629d4bd2eb9409fadf602ec03
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-27 14:15:24 -0700

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,7 +17,7 @@ def close_storage(exeption):
 @app.errorhandler(404)
 def page_not_found(error):
     """“404 page”, a “Not found"""
-    return jsonif({
+    return jsonify({
         "error": "Not found"
         }), 404
 
