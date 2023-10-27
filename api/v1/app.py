610d29ee7eb3c0a1111468653194e9a96797110a
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-27 13:40:14 -0700

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,14 @@ def close_storage(exeption):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """“404 page”, a “Not found"""
+    return jsonif({
+        "error": "Not found"
+        }), 404
+
+
 if __name__ == "__main__":
     host = '0.0.0.0' if not getenv('HBNB_API_HOST') else getenv('HBNB_API_HOST')
     port = 5000 if not getenv('HBNB_API_PORT') else int(getenv('HBNB_API_PORT'))
