3966d29ba240062e6d05e83956adf6a93d9318f3
laila22haz <hazmirlaila316@gmail.com>
2023-10-28 17:52:47 +0100

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-from flask import Flask
+from os import getenv
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask import make_response
-from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_storage(exeption):
+def close_storage_exception(exception):
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """“404 page”, a “Not found"""
-    return make_response(jsonify({
-        "error": "Not found"
-        }), 404)
+    """
+    Handler for 404 errors.
+    Returns a response indicating 'Not found'.
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
