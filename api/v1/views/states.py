e547652d1bb6f64d12a476184dec3635da438918
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-28 14:58:34 -0700

    task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
+""" state doc"""
+
 from models.base_model import BaseModel
 from models import storage
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models.state import State
-from flask import request
 
 
-@app_views.route('/api/v1/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def all_states():
+    """doc fuction"""
     states_list = []
     states = storage.all(State).values()
     for state in states:
         states_list.append(state.to_dict())
     return jsonify(states_list)
 
-@app_views.route('/api/v1/states/<state_id>', methods=['GET'])
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
+    """get states"""
     state = storage.get(State, state_id)
     if state is None:
-        abort(404)
+        abort(404, 'Not found')
     else:
         return jsonify(state.to_dict())
 
-@app_views.route('/api/v1/states/<state_id>', methods=['DELETE'])
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
+    """ doc delete"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
@@ -33,8 +40,10 @@ def delete_state(state_id):
         storage.save()
         return jsonify({}, 200)
 
-@app_views.route('/api/v1/states', methods=['POST'])
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
+    """doc create"""
     request_data = request.get_json()
     if not request_data:
         abort(400, 'Not a JSON')
@@ -45,8 +54,10 @@ def create_state():
     storage.save()
     return jsonify(new_state.to_dict, 201)
 
-@app_views.route('/api/v1/states/<state_id>', methods=['PUT'])
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
+    """doc update"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
