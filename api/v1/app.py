16b62e14c793db2aa63da4864b18b6ca581d4356
laila22haz <hazmirlaila316@gmail.com>
2023-10-28 18:29:01 +0100

    task 5 requirement

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""app file"""
-from flask import app, Flask, make_response, jsonify, abort, Blueprint
+"""API file to define the blueprint for endpoints """
+from flask import Flask, jsonify, abort, make_response, Blueprint
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
 app = Flask(__name__)
 
@@ -17,12 +17,12 @@ def teardown(exception):
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """Handler for 404 errors"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+def not_found(error):
+    """handler for 404 errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', "0.0.0.0")
-    port = int(getenv('HBNB_API_PORT', "0.0.0.0"))
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
