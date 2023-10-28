f6eba9336571d537b8c1d24c4ef1e530afe24e2b
laila22haz <hazmirlaila316@gmail.com>
2023-10-28 13:09:11 +0100

    task 6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def stats():
+    count_object = {}
+    count_object['amenities'] = storage.count(Amenity)
+    count_object['cities'] = storage.count(City)
+    count_object['places'] = storage.count(Place)
+    count_object['reviews'] = storage.count(Review)
+    count_object['states'] = storage.count(State)
+    count_object['users'] = storage.count(User)
+    return jsonify(count_object)
---
+++ api/v1/views/states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+from models import storage
+from flask import jsonify, abort
+from api.v1.views import app_views
+from models.state import State
+from flask import request
+
+
+@app_views.route('/api/v1/states', methods=['GET'])
+def all_states():
+    states_list = []
+    states = storage.all(State).values()
+    for state in states:
+        states_list.append(state.to_dict())
+    return jsonify(states_list)
+
+@app_views.route('/api/v1/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    else:
+        return jsonify(state.to_dict())
+
+@app_views.route('/api/v1/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    else:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}, status=200)
+
+@app_views.route('/api/v1/states', methods=['POST'])
+def create_state():
+    request_data = request.get_json()
+    if not request_data:
+        abort(400, 'Not a JSON')
+    if 'name' not in request_data:
+        abort(400, 'Missing name')
+    new_state = State(request_data['name'])
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict, status=201)
+
